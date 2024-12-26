4b89467fccc2d531e44589aa0b15872b16d7c35a
raheel <raheel@vrox.co.uk>
2024-12-26 17:52:40 +0500

    fixed home

--- apiconfig.js
+++ apiconfig.js
@@ -1,9 +1,4 @@
 const ApiData = {
-  url: "https://web.fvsols.com",
-  // url: "https://api.websouls.com",
   // url: "http://192.168.18.41:5001/",
-  STRIPE_PUBLIC_KEY:
-    "pk_test_51ON9CRDwRUAgiWoliMeRJc1oord0LvEcJQu5bLqnv8AQBBe2S4QlXEPNbKDA2ODWpoeMFfQmHVx3sJ5rUqgmrXiq00UTQsCN1P",
-    // "pk_live_51JUdjTKFefZ7VaeK8AOF4xiKytIEFQg3f04k1hkPTPm3aYwUTmi7qt9lGu4wDRtMWEuCob2u2LGlI0E5JZjoT4Pc00xhcO0f1E",
 };
 export default ApiData;
